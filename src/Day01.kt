325f3b5fb345366b97a53e246bb41fafba4a709a
Lucas Geisink <485717@student.saxion.nl>
2021-12-01 11:37:32 +0100

    Add Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var largerThenCounter = 0
+        for((index, value) in input.withIndex()) {
+            if(index != 0) {
+                if(input[index - 1].toInt() < value.toInt()) largerThenCounter++
+            }
+        }
+        return largerThenCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val listOfSums: MutableList<String> = mutableListOf()
+        for((index, number) in input.withIndex()) {
+            if(index + 2 > input.size - 1) {
+                break
+            }
+            val sum = number.toInt() + input[index + 1].toInt() + input[index + 2].toInt()
+            listOfSums.add(sum.toString())
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return part1(listOfSums)
+    }
 
     val input = readInput("Day01")
     println(part1(input))
